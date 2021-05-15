103394a2777388c4a711cdff2faada32e4413e08
Saurabh Mehta <soru.mehta@gmail.com>
2021-05-15 21:33:14 +0530

    meteor todo tut with with graphql

---
+++ imports/ui/TaskForm.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { TasksCollection } from '/imports/db/TasksCollection';
+import { useMutation } from "@apollo/react-hooks";
+import gql from 'graphql-tag';
+
+const taskMutation =  gql`
+  mutation AddTask($text: String!) {
+    addTask(text: $text) {
+      _id
+    }
+  }
+`
+
+export const TaskForm = () => {
+    const [addTaskMutation] = useMutation(taskMutation);
+    const [text, setText] = useState("");
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+  
+      if (!text) return;
+
+      addTaskMutation({
+        variables: {
+          text,
+        },
+        refetchQueries: () => ['Tasks']
+      })
+        .then(() => console.log('Task added with success'))
+        .catch(e => console.error('Error trying to add task', e));
+  
+      setText("");
+    };
+   
+ 
+    return (
+        <form className="task-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Type to add new tasks"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+     
+          <button type="submit">Add Task</button>
+        </form>
+      );
+};
